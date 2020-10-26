09c520580b1ac9132a0216311c6e608350897b00
SebastianAndresen <sebastian.sommersted@live.dk>
2020-10-26 14:19:39 +0100

    serverless hosting test

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Script is loaded");
+
+//import { H5P } from 'h5p-standalone'; // ES6
+//const { H5P } = require('h5p-standalone'); //AMD
+// <script src="node_modules/h5p-standalone/dist/main.bundle.js"> // Globals
+//const { H5P } = 'H5PStandalone';
+
+const el = document.getElementById('h5p-container');
+const h5pLocation = './workspace';
+
+const h5p = new H5P(el, h5pLocation);
